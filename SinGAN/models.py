929833eda35ab86ed73c53c301d1b398d2556f23
ilyak93 <ilyak93@gmail.com>
2021-03-25 12:35:29 +0200

    fix

--- SinGAN/models.py
+++ SinGAN/models.py
@@ -485,10 +485,76 @@ class ImgTransfromerGeneratorConcatSkip2CleanAdd(nn.Module):
         
 
         
-from performer_pytorch import SelfAttention
+from performer_pytorch import SelfAttention, Performer
 
 
 class PerformerSAWDiscriminator(nn.Module):
+    def __init__(self, opt):
+        super(PerformerSAWDiscriminator, self).__init__()
+        self.is_cuda = torch.cuda.is_available()
+        N = int(opt.nfc)
+        self.head = ConvBlock(opt.nc_im, N, opt.ker_size, opt.padd_size, 1)
+        self.body = nn.Sequential()
+        for i in range(opt.num_layer - 2):
+            N = int(opt.nfc / pow(2, (i + 1)))
+            block = ConvBlock(max(2 * N, opt.min_nfc), max(N, opt.min_nfc), opt.ker_size, opt.padd_size, 1)
+            self.body.add_module('block%d' % (i + 1), block)
+        if opt.attn == True:
+            self.attn = SelfAttention(
+                    dim = max(N, opt.min_nfc),
+                    heads = 4,
+                    causal = False
+                    )
+            self.gamma = nn.Parameter(torch.zeros(1))
+        self.tail = nn.Conv2d(max(N, opt.min_nfc), 1, kernel_size=opt.ker_size, stride=1, padding=opt.padd_size)
+
+    def forward(self, x):
+        x = self.head(x)
+        x = self.body(x)
+        if hasattr(self, 'attn'):
+            x = self.gamma * self.attn(x) + x
+        x = self.tail(x)
+        return x
+
+
+class PerformerSAGeneratorConcatSkip2CleanAdd(nn.Module):
+    def __init__(self, opt):
+        super(PerformerSAGeneratorConcatSkip2CleanAdd, self).__init__()
+        self.is_cuda = torch.cuda.is_available()
+        N = opt.nfc
+        self.head = ConvBlock(opt.nc_im, N, opt.ker_size, opt.padd_size,
+                              1)  # GenConvTransBlock(opt.nc_z,N,opt.ker_size,opt.padd_size,opt.stride)
+        self.body = nn.Sequential()
+        for i in range(opt.num_layer - 2):
+            N = int(opt.nfc / pow(2, (i + 1)))
+            block = ConvBlock(max(2 * N, opt.min_nfc), max(N, opt.min_nfc), opt.ker_size, opt.padd_size, 1)
+            self.body.add_module('block%d' % (i + 1), block)
+        if opt.attn == True:
+            self.attn = SelfAttention(
+                    dim = max(N, opt.min_nfc),
+                    heads = 4,
+                    causal = False
+                    )
+            self.gamma = nn.Parameter(torch.zeros(1))
+        self.tail = nn.Sequential(
+            nn.Conv2d(max(N, opt.min_nfc), opt.nc_im, kernel_size=opt.ker_size, stride=1, padding=opt.padd_size),
+            nn.Tanh()
+        )
+
+    def forward(self, x, y):
+        x = self.head(x)
+        x = self.body(x)
+        if hasattr(self, 'attn'):
+            x = self.gamma * self.attn(x) + x
+        x = self.tail(x)
+        ind = int((y.shape[2] - x.shape[2]) / 2)
+        y = y[:, :, ind:(y.shape[2] - ind), ind:(y.shape[3] - ind)]
+        return x + y
+		
+		
+		
+		
+class PerformerWDiscriminator(nn.Module):
     def __init__(self, opt):
         super(AxialWDiscriminator, self).__init__()
         self.is_cuda = torch.cuda.is_available()
@@ -517,7 +583,7 @@ class PerformerSAWDiscriminator(nn.Module):
         return x
 
 
-class PerformerSAGeneratorConcatSkip2CleanAdd(nn.Module):
+class PerformerGeneratorConcatSkip2CleanAdd(nn.Module):
     def __init__(self, opt):
         super(AxialGeneratorConcatSkip2CleanAdd, self).__init__()
         self.is_cuda = torch.cuda.is_available()
