74438377af589be01c04f2178ff8dd8d3f48323d
ilyak93 <ilyak93@gmail.com>
2021-03-26 07:07:12 +0300

    RESTORE back after that commit
    many change in training. Just for the experiment.

--- SinGAN/models.py
+++ SinGAN/models.py
@@ -638,3 +638,139 @@ class PerformerGeneratorConcatSkip2CleanAdd(nn.Module):
         return x + y
 
 
+        
+        
+        
+
+        
+        
+        
+        
+
+from torch import nn, einsum
+import torch.nn.functional as F
+
+from einops import rearrange, repeat
+from einops.layers.torch import Rearrange
+
+class Residual(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+    def forward(self, x, **kwargs):
+        return self.fn(x, **kwargs) + x
+
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, hidden_dim, dropout = 0.):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout(dropout)
+        )
+    def forward(self, x):
+        return self.net(x)
+
+class Attention(nn.Module):
+    def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
+        super().__init__()
+        inner_dim = dim_head * heads
+        project_out = not (heads == 1 and dim_head == dim)
+
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim),
+            nn.Dropout(dropout)
+        ) if project_out else nn.Identity()
+
+    def forward(self, x):
+        b, n, _, h = *x.shape, self.heads
+        qkv = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = h), qkv)
+
+        dots = einsum('b h i d, b h j d -> b h i j', q, k) * self.scale
+
+        attn = dots.softmax(dim=-1)
+
+        out = einsum('b h i j, b h j d -> b h i d', attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        out =  self.to_out(out)
+        return out
+
+class Transformer(nn.Module):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout = 0.):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                Residual(PreNorm(dim, Attention(dim, heads = heads, dim_head = dim_head, dropout = dropout))),
+                Residual(PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout)))
+            ]))
+    def forward(self, x):
+        for attn, ff in self.layers:
+            x = attn(x)
+            x = ff(x)
+        return x
+
+class ViT(nn.Module):
+    def __init__(self, *, image_size, patch_size=[4,4], embed_dim = 48, depth=1, heads=4, mlp_dim =96, channels = 3, dim_head = 64, dropout = 0., emb_dropout = 0., output_dim=3):
+        super().__init__()
+
+        self.btm_pad = - image_size[0] % patch_size[0]
+        self.rht_pad = - image_size[1] % patch_size[1]
+        self.pad_input = nn.ZeroPad2d((0, self.rht_pad, 0, self.btm_pad))
+
+        img_size = image_size[0] + self.btm_pad, image_size[1] + self.rht_pad
+
+        patch_dim = channels*patch_size[0]*patch_size[1]
+        num_patches = (img_size[1] // patch_size[1]) * (img_size[0] // patch_size[0])
+
+
+        self.to_patch_embedding = nn.Sequential(
+            Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = patch_size[0], p2 = patch_size[1]),
+            nn.Linear(patch_dim, embed_dim),
+        )
+
+        self.pos_embedding = nn.Parameter(torch.randn(1, num_patches, embed_dim))
+
+        self.dropout = nn.Dropout(emb_dropout)
+        dim_head = embed_dim // heads
+        self.transformer = Transformer(embed_dim, depth, heads, dim_head, mlp_dim, dropout)
+
+        self.to_latent = nn.Identity()
+
+        h = img_size[0] // patch_size[0]
+        w = img_size[1] // patch_size[1]
+        patch_dim = (patch_dim // channels) * output_dim
+        self.from_patch_embedding = nn.Sequential(
+            nn.Linear(embed_dim, patch_dim),
+            Rearrange('b (h w) (p1 p2 c) -> b c (h p1) (w p2)', h=h, w=w, p1=patch_size[0], p2=patch_size[1], c=output_dim)
+        )
+
+
+    def forward(self, img):
+        shape = img.shape
+        img = self.pad_input(img)
+        x = self.to_patch_embedding(img)
+        b, n, _ = x.shape
+
+        x += self.pos_embedding[:, :n]
+        x = self.dropout(x)
+
+        x = self.transformer(x)
+
+        return self.from_patch_embedding(x)[:,:,:shape[2], :shape[3]]
