efad11c3c0d081b3eff2dab9779bc9db79ac283b
ilyak93 <ilyak93@gmail.com>
2021-03-26 20:13:15 +0300

    Update models.py

--- SinGAN/models.py
+++ SinGAN/models.py
@@ -716,7 +716,7 @@ class Transformer(nn.Module):
     def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout = 0.):
         super().__init__()
         self.layers = nn.ModuleList([])
-		self.gamma = nn.Parameter(torch.zeros(1))
+        self.gamma = nn.Parameter(torch.zeros(1))
         for _ in range(depth):
             self.layers.append(nn.ModuleList([
                 Residual(PreNorm(dim, Attention(dim, heads = heads, dim_head = dim_head, dropout = dropout))),
