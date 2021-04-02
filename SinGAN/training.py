42f035a3862fafca9fa0762648bd79c3d0cabca1
ilyak93 <ilyak93@gmail.com>
2021-04-02 23:08:31 +0300

    Update training.py

--- SinGAN/training.py
+++ SinGAN/training.py
@@ -387,7 +387,7 @@ def train_paint(opt,Gs,Zs,reals,NoiseAmp,centers,paint_inject_scale):
 def init_models(opt):
 
     #generator initialization:
-    netG = models.AxialGeneratorConcatSkip2CleanAdd5(opt).to(opt.device)
+    netG = models.AxialGeneratorConcatSkip2CleanAdd2(opt).to(opt.device)
     netG.apply(models.weights_init)
     if opt.netG != '':
         netG.load_state_dict(torch.load(opt.netG))
@@ -398,7 +398,7 @@ def init_models(opt):
     
 
     #discriminator initialization:
-    netD = models.AxialWDiscriminator5(opt).to(opt.device)
+    netD = models.AxialWDiscriminator2(opt).to(opt.device)
     netD.apply(models.weights_init)
     if opt.netD != '':
         netD.load_state_dict(torch.load(opt.netD))
