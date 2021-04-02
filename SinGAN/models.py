d62aa5b583cb4ae8409bb1cb48b8e685d162d6b1
ilyak93 <ilyak93@gmail.com>
2021-04-02 14:51:26 +0300

    Update models.py

--- SinGAN/models.py
+++ SinGAN/models.py
@@ -667,7 +667,7 @@ class AxialWDiscriminator6(nn.Module):
                 dim=3,  # embedding dimension
                 dim_index=1,  # where is the embedding dimension
                 # dim_heads = 32,        # dimension of each head. defaults to dim // heads if not supplied
-                heads=4,  # number of heads for multi-head attention
+                heads=3,  # number of heads for multi-head attention
                 num_dimensions=2,  # number of axial dimensions (images is 2, video is 3, or more)
                 sum_axial_out=True
                 # whether to sum the contributions of attention on each axis, or to run the input through them sequentially. defaults to true
